c81bc1a8d19da400c7ea3e6cf3c8883971128ea6
jayce <jayce404@foxmail.com>
2021-03-21 16:55:56 +0800

    组件的slot属性

--- my-project-components/src/components/TestComponents.vue
+++ my-project-components/src/components/TestComponents.vue
@@ -5,15 +5,26 @@
             :text_color="post.color"
             :wight="post.wight"
         />
+        <slot-demo1>
+            这是slot的属性，方便父组件插入内容
+        </slot-demo1>
+
+        <slot-demo2>
+            <p slot="slot_top">这是slot的头部</p>
+<!--            <p>这是未定义的slot</p>-->
+            <h5 slot="slot_bottom">这是slot的尾部</h5>
+        </slot-demo2>
     </div>
 </template>
 
 <script>
 import BlogPost from "@/components/blog-post/BlogPost";
+import SlotDemo1 from "@/components/slot-demo/slotDemo1";
+import SlotDemo2 from "@/components/slot-demo/slotDemo2";
 
 export default {
     components: {
-        BlogPost,
+        BlogPost, SlotDemo1, SlotDemo2,
     },
     data() {
         return {
---
+++ my-project-components/src/components/slot-demo/slotDemo2.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <slot name="slot_top"></slot>
+        <slot>这里是slot的默认值</slot>
+        <slot name="slot_bottom"></slot>
+    </div>
+</template>
