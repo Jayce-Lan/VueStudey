9b97f7822dd4a0764d76b18431fab9baf039f2b7
jayce <jayce404@foxmail.com>
2021-03-21 16:30:44 +0800

    组件之间的调用

---
+++ my-project-components/src/components/TestComponents.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <blog-post
+            :title="post.title"
+            :text_color="post.color"
+            :wight="post.wight"
+        />
+    </div>
+</template>
+
+<script>
+import BlogPost from "@/components/blog-post/BlogPost";
+
+export default {
+    components: {
+        BlogPost,
+    },
+    data() {
+        return {
+            post: {
+                title: "这是一个标题",
+                color: "blue",
+                wight: 10,
+                sex: "man",
+            }
+        }
+    }
+}
+</script>
---
+++ my-project-components/src/components/blog-post/BlogPost.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{title}}</h2>
+        <p :style="'color:' + text_color">{{wight}}</p>
+        <p>{{sex}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        text_color: String,
+        wight: Number,
+        sex: {
+            type: String,
+            default: "man",
+        }
+    }
+}
+</script>
