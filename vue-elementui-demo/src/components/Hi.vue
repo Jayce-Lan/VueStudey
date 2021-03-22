04571af16666dd036274f0d157c2bb0019713453
jayce <jayce404@foxmail.com>
2021-03-22 21:14:24 +0800

    整合ElementUI

---
+++ vue-elementui-demo/src/components/Hi.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="hi">
+        <el-container>
+            <el-header style="background: brown">Header</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <NavMenu :items="testValue" :title="'标题'" :active="clickNav"></NavMenu>
+                </el-aside>
+                <el-main style="background: darkgray">Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+import NavMenu from "@/components/NavMenu/NavMenu";
+export default {
+    name: 'hi',
+    components: {
+        NavMenu,
+    },
+    data() {
+        return {
+            testValue: [
+                {id: 1, name: "test1"},
+                {id: 2, name: "test2"},
+                {id: 3, name: "test3"},
+                {id: 4, name: "test4"},
+            ]
+        }
+    },
+    methods: {
+        clickNav() {
+            // console.log(value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-elementui-demo/src/components/NavMenu/NavMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-menu :default-openeds="['1', '3']">
+        <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>{{ title }}</template>
+            <el-menu-item-group v-for="item in items" :key="item.id">
+                <el-menu-item style="color:#000;"
+                              index="item.id"
+                              @click="handClick(item.id)">
+                    {{ item.name }}
+                </el-menu-item>
+            </el-menu-item-group>
+        </el-submenu>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        items: []
+    },
+    methods: {
+        handClick(value) {
+            this.$emit("active")
+            console.log(value);
+        }
+    }
+}
+</script>
